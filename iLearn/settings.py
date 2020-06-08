1e811882bfa896c236b5a180c625b71cfe18cdbd
Patrycja <patrycjatyra@protonmail.com>
2020-06-08 18:01:52 +0000

    added authentication backends to settings.py

--- iLearn/settings.py
+++ iLearn/settings.py
@@ -59,7 +59,7 @@ TEMPLATES = [
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
-                'django.template.context_processors.request',
+                'django.template.context_processors.request', # required by allauth
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
@@ -67,7 +67,17 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'iLearn.wsgi.application'
+AUTHENTICATION_BACKENDS = [
+    # Needed to login by username in Django admin, regardless of `allauth`
+    'django.contrib.auth.backends.ModelBackend',
+
+    # `allauth` specific authentication methods, such as login by e-mail
+    'allauth.account.auth_backends.AuthenticationBackend',
+    ...
+]
+
+
+WSGI_APPLICATION = 'iLearn.wsgi.application
 
 
 # Database
