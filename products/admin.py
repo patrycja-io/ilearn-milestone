18c52658228b1c24be4eaa7d709f23b752938842
Patrycja <patrycjatyra@protonmail.com>
2020-07-16 13:50:46 +0000

    added changes to admin.py

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,24 @@
 from django.contrib import admin
 from .models import Product, Category
 
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
 # Register your models here.
 admin.site.register(Product)
 admin.site.register(Category)
