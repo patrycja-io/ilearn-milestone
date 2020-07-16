cc50baf7355e713b57dcb4a34f4e33ba0380bbea
Patrycja <patrycjatyra@protonmail.com>
2020-07-16 13:50:33 +0000

    added changes to urls

--- iLearn/urls.py
+++ iLearn/urls.py
@@ -23,4 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
+    path('profile/', include('home.urls')),
+ 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
