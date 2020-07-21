b8d1e68eacf946c8c153e1b9aaec8182d3726bc0
Patrycja <patrycjatyra@protonmail.com>
2020-07-21 17:20:59 +0000

    added search engine

--- products/views.py
+++ products/views.py
@@ -3,24 +3,37 @@ from django.contrib import messages
 from django.db.models import Q
 from .models import Product
 
-# Create your views here.
-    def all_products(request):
-    """ """
-        products = Product.objects.all()
 
-        if request.GET:
-            if 'q' in request.GET:
-                query = request.GET['q']
-                if not query:
-                    messages.error(request, "You didn't enter any search criteria!")
-                    return redirect(reverse('products'))
+def courses_all(request):
+    """ A view to show all products, including sorting and search queries """
+    products = Product.objects.all()
+    query = None
 
-    context = { 
-        'products' : products,
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('products'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
 
+    context = {
+        'products': products,
+        'search_term': query,
     }
-
     return render(request, 'products/products.html', context)
 
+
 def product_detail(request, product_id):
+    """ A view to show individual product details """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
     
