fba39b2fe79a5f26098bf138afdd7d62dbbd71dd
Patrycja <patrycjatyra@protonmail.com>
2020-07-22 14:53:17 +0000

    added courses categories to def all courses

--- products/views.py
+++ products/views.py
@@ -1,39 +1,46 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product
+from .models import Course, Category
 
 
 def courses_all(request):
     """ A view to show all products, including sorting and search queries """
-    products = Product.objects.all()
+    courses = Course.objects.all()
     query = None
+    categories = None
 
     if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            courses = courses.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
                 messages.error(request, "You didn't enter any search criteria!")
-                return redirect(reverse('products'))
+                return redirect(reverse('courses'))
             
             queries = Q(name__icontains=query) | Q(description__icontains=query)
-            products = products.filter(queries)
+            courses = courses.filter(queries)
 
     context = {
-        'products': products,
+        'courses': courses,
         'search_term': query,
+        'current_categories': categories,
     }
-    return render(request, 'products/products.html', context)
+    return render(request, 'courses/courses.html', context)
 
 
-def product_detail(request, product_id):
-    """ A view to show individual product details """
+def course_detail(request, course_id):
+    """ A view to show individual course details """
 
-    product = get_object_or_404(Product, pk=product_id)
+    course = get_object_or_404(Course, pk=course_id)
 
     context = {
-        'product': product,
+        'course': course,
     }
 
-    return render(request, 'products/product_detail.html', context)
+    return render(request, 'courses/course_detail.html', context)
     
