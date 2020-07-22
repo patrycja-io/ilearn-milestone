f1255f4909d631598c67d3d3ffda3f6943779a47
Patrycja <patrycjatyra@protonmail.com>
2020-07-22 15:56:56 +0000

    added sorting to views.py

--- products/views.py
+++ products/views.py
@@ -9,8 +9,23 @@ def courses_all(request):
     courses = Course.objects.all()
     query = None
     categories = None
+    sort = None
+    direction = None
 
     if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                courses = courses.annotate(lower_name=Lower('name'))
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            courses = courses.order_by(sortkey)
+
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             courses = courses.filter(category__name__in=categories)
@@ -25,10 +40,14 @@ def courses_all(request):
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             courses = courses.filter(queries)
 
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         'courses': courses,
         'search_term': query,
         'current_categories': categories,
+        'current_sorting': current_sorting,
+        
     }
     return render(request, 'courses/courses.html', context)
 
