7186b5c119b1dda10185c1e8988eab827ec0e5fc
Patrycja <patrycjatyra@protonmail.com>
2020-08-02 22:10:25 +0000

    added django to lower

--- courses/views.py
+++ courses/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
+from django.contrib.auth.decorators import login_required
+
 from .models import Course, Category
+
 
 
 def all_courses(request):
