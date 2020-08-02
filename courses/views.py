8fdf4ccb223d71b0018433c572952a92e979a971
Patrycja <patrycjatyra@protonmail.com>
2020-08-02 22:10:37 +0000

    added login required

--- courses/views.py
+++ courses/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
+from django.db.models.functions import Lower
 from django.contrib.auth.decorators import login_required
 
 from .models import Course, Category
