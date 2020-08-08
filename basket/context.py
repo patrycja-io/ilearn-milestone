98f95bf8e05ab86186955ae7a26a8977ad18f158
Patrycja <patrycjatyra@protonmail.com>
2020-08-08 11:51:44 +0000

    added shopping cart calculation

---
+++ basket/context.py
@@ -0,0 +1,17 @@
+from decimal import Decimal
+from django.conf import settings
+
+def basket_ebooks(request):
+
+    basket_items = []
+    total = 0
+    product_count = 0
+    
+    context = {
+        'basket_items': bag_items,
+        'total': total,
+        'course_count': product_count,
+        'grand_total': grand_total,
+    }
+
+    return context
