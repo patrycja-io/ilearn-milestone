c2769c5904313161ea8076047f86a8e2a849f280
Patrycja <patrycjatyra@protonmail.com>
2020-08-08 19:10:53 +0000

    changed linting in context.py

--- basket/context.py
+++ basket/context.py
@@ -1,16 +1,17 @@
 from decimal import Decimal
 from django.conf import settings
 
+
 def basket_ebooks(request):
 
     basket_items = []
     total = 0
     product_count = 0
-    
-    grand_total =  total
-    
+
+    grand_total = total
+
     context = {
-        'basket_items': bag_items,
+        'basket_items': basket_items,
         'total': total,
         'course_count': product_count,
         'grand_total': grand_total,
