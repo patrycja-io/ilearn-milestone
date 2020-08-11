8bd6bc2a120f62abda27d1d342972a920463258d
Patrycja <patrycjatyra@protonmail.com>
2020-08-11 21:54:28 +0000

    added function add to basket

--- basket/views.py
+++ basket/views.py
@@ -5,3 +5,13 @@ def view_basket(request):
     """ A view  that renders basket page """
 
     return render(request, 'basket/basket.html')
+
+def add_to_basket(request, item_id):
+    """ Add a quantity of the specified product to the shopping basket """
+
+    redirect_url = request.POST.get('redirect_url')
+    basket = request.session.get('basket', {}) 
+
+    request.session['basket'] = basket
+    print(request.session['basket'])
+    return redirect(redirect_url)   
