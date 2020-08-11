b441dd72088fc94748d8b55c817f255798a59b02
Patrycja <patrycjatyra@protonmail.com>
2020-08-11 22:03:26 +0000

    added urls add to bag

--- basket/urls.py
+++ basket/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 from .import views
 
 urlpatterns = [
-    path('', views.view_basket, name='view_basket')
+    path('', views.view_basket, name='view_basket'),
+    path('add/<item_id>/', views.add_to_basket, name='add_to_basket'),
 ]
