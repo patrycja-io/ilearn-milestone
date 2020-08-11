7c8aea9b0ccb244bab7d2470be6a5675c40d89ad
Patrycja <patrycjatyra@protonmail.com>
2020-08-11 22:29:26 +0000

    updated basket context a

--- basket/context.py
+++ basket/context.py
@@ -1,5 +1,8 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from courses.models import Course
+
 
 
 def basket_ebooks(request):
@@ -7,6 +10,14 @@ def basket_ebooks(request):
     basket_items = []
     total = 0
     product_count = 0
+    basket = request.session.get('basket', {})
+
+    for item_id in basket.items():
+        course = get_object_or_404(Course, pk=item_id)
+        basket_items.append({
+            'item_id': item_id,
+            'course': course,
+        })
 
     grand_total = total
 
