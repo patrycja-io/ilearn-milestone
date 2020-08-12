f36f9121b99d176539731e6067b7d7a39c53aa69
Patrycja <patrycjatyra@protonmail.com>
2020-08-12 18:05:48 +0000

    updated name

--- basket/urls.py
+++ basket/urls.py
@@ -4,5 +4,5 @@ from .import views
 
 urlpatterns = [
     path('', views.view_basket, name='view_basket'),
-    path('add/<item_id>/', views.add_to_basket, name='add_to_basket'),
+    path('add/<course_id>/', views.add_to_basket, name='add_to_basket'),
 ]
