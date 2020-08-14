60754e6fab0b003705a69b4eb809086c6f4b89d2
Patrycja <patrycjatyra@protonmail.com>
2020-08-14 14:29:37 +0000

    updated key

--- ilearn/settings.py
+++ ilearn/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 
-SECRET_KEY = od.environ.get('SECRET_KEY', '')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['ilearn-django.herokuapp.com','localhost']
 
