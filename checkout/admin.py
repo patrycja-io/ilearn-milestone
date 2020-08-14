d8a530abf997f341043b194552f89c4227e3f195
Patrycja <patrycjatyra@protonmail.com>
2020-08-14 12:14:43 +0000

    added admin to fields in checkout

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Order, OrderItem
 
-# Register your models here.
+
+class OrderItemAdminInline(admin.TabularInline):
+    model = OrderItem
+    
+
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (OrderItemAdminInline,)
+
+
+    fields = ('order_number', 'date', 'name', 'surname',
+              'email', 'phone_number', 'country',
+              'postcode', 'town_or_city', 'street_address1',
+              'street_address2', 'county',
+              'total',)
+
+    list_display = ('order_number', 'date', 'name', 'surname',
+                    'total',)
+
+    ordering = ('-date',)
+
+
+admin.site.register(Order, OrderAdmin)
