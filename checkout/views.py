a11c58d7be54147e51b81bfdac26cf29943b0168
Patrycja <patrycjatyra@protonmail.com>
2020-08-14 13:38:08 +0000

    added good paths to template

--- checkout/views.py
+++ checkout/views.py
@@ -11,7 +11,7 @@ def checkout(request):
         return redirect(reverse('courses'))
 
     order_form = OrderForm()
-    template = 'checkout/checkout.html'
+    template = 'checkout.html'
     context = {
         'order_form': order_form,
     }
