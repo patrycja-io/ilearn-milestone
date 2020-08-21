fab16bf592738d540ff899bc59d14b60c4d30f6d
Patrycja <patrycjatyra@protonmail.com>
2020-08-21 17:24:35 +0000

    updated basket

--- ilearn/settings.py
+++ ilearn/settings.py
@@ -81,7 +81,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.media',
-                'bag.contexts.bag_contents',
+                'basket.context.basket_ebooks',
             ],
             'builtins': [
                 'crispy_forms.templatetags.crispy_forms_tags',
--- ilearn/urls.py
+++ ilearn/urls.py
@@ -26,6 +26,4 @@ urlpatterns = [
     path('basket/', include('basket.urls')),
     path('checkout/', include('checkout.urls')),
     
-
-
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
