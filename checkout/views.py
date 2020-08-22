4ec23bff860eb7938b9e560bd46b932d028c60ab
Patrycja <patrycjatyra@protonmail.com>
2020-08-22 20:49:56 +0000

    added different stripe key

--- checkout/views.py
+++ checkout/views.py
@@ -35,8 +35,8 @@ def checkout(request):
     template = 'checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': stripe_public_key,
-        'client_secret': intent.client_secret,
+        'stripe_public_key': 'pk_test_51HHsLPHe52J6RnjHdH4B4wyvSY2jVlZxZeIABYuPmMtXHJ84S6cW7AruDORAxHFW7eUpcpVjwwTGQR0nPxBk7RXZ00diHfkSEQ',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
