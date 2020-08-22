e48ed43bb3d36c44f4bfdc224822a44a92108a5d
Patrycja <patrycjatyra@protonmail.com>
2020-08-22 20:49:19 +0000

    added update to country field

--- checkout/models.py
+++ checkout/models.py
@@ -2,18 +2,22 @@ import uuid
 
 from django.db import models
 from django.db.models import Sum
-from django.conf import settings
+
+from django_countries.fields import CountryField
 
 from courses.models import Course
+from accounts.models import UserAccount
 
 
 class Order(models.Model):
     order_number = models.CharField(max_length=32, null=False, editable=False)
+    user_account = models.ForeignKey(UserAccount, on_delete=models.SET_NULL,
+                                     null=True, blank=True, related_name='orders')
     name = models.CharField(max_length=50, null=False, blank=False)
     surname = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
-    country = models.CharField(max_length=40, null=False, blank=False)
+    country = CountryField(blank_label='Country *', null=False, blank=False)
     postcode = models.CharField(max_length=20, null=True, blank=True)
     town_or_city = models.CharField(max_length=40, null=False, blank=False)
     street_address1 = models.CharField(max_length=80, null=False, blank=False)
@@ -50,9 +54,12 @@ class Order(models.Model):
 
 
 class OrderItem(models.Model):
-    order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='items')
-    course = models.ForeignKey(Course, null=False, blank=False, on_delete=models.CASCADE)
-    item_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
+    order = models.ForeignKey(Order, null=False, blank=False, 
+        on_delete=models.CASCADE, related_name='items')
+    course = models.ForeignKey(Course, null=False, blank=False,
+        on_delete=models.CASCADE)
+    item_total = models.DecimalField(max_digits=6, decimal_places=2, 
+        null=False, blank=False, editable=False)
 
     def save(self, *args, **kwargs):
         """
