860eaab45adbb936a0070b5d7ae10a11e37ad4b2
Patrycja <patrycjatyra@protonmail.com>
2020-08-22 20:49:34 +0000

    updated signal

--- checkout/signals.py
+++ checkout/signals.py
@@ -3,15 +3,17 @@ from django.dispatch import receiver
 
 from .models import OrderLineItem
 
+
 @receiver(post_save, sender=OrderLineItem)
-def update_on_save(sender, instance, created, **kwargs):
+def update_save(sender, instance, created, **kwargs):
     """
     Update order total on lineitem update/create
     """
     instance.order.update_total()
 
+
 @receiver(post_delete, sender=OrderLineItem)
-def update_on_save(sender, instance, **kwargs):
+def update_delete(sender, instance, **kwargs):
     """
     Update order total on lineitem delete
     """
