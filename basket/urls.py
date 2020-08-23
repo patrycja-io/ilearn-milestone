b4e7ed17368ab1617eeb1f502fe8e06c27002c82
Patrycja <patrycjatyra@protonmail.com>
2020-08-23 21:25:17 +0000

    changed linting in urls

--- basket/urls.py
+++ basket/urls.py
@@ -5,5 +5,6 @@ from .import views
 urlpatterns = [
     path('', views.view_basket, name='view_basket'),
     path('add/<course_id>/', views.add_to_basket, name='add_to_basket'),
-    path('delete/<course_id>/', views.delete_from_basket, name='delete_from_basket'),
+    path('delete/<course_id>/', views.delete_from_basket,
+         name='delete_from_basket'),
 ]
