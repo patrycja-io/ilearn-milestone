61de0e4cbf3878d45310a40c23d87122ccac7450
Patrycja <patrycjatyra@protonmail.com>
2020-08-24 21:13:19 +0000

    added different logic fixed bug

--- basket/views.py
+++ basket/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
+
+from courses.models import Course
 
 
 def view_basket(request):
@@ -11,25 +14,28 @@ def add_to_basket(request, course_id):
     """ Add a quantity of the specified product to the shopping basket """
     redirect_url = request.POST.get('redirect_url')
     basket = request.session.get('basket', {})
+    course = Course.objects.get(pk=course_id)
 
     if course_id not in basket:
         basket[course_id] = course_id
+
+    else:
+        basket[course_id] = course_id
+        messages.success(request, f'Added {course.name} to your basket')
 
     request.session['basket'] = basket
     return redirect(redirect_url)
 
 
 def delete_from_basket(request, course_id):
-    """
-    Delete item from cart
-    """
+    """Delete item from cart"""
+
+    course = Course.objects.get(pk=course_id)
     basket = request.session.get('basket', {})
+
     if course_id in basket:
         del basket[course_id]
-        messages.info(request, "Item has been deleted")
+        messages.success(request, f'Removed {course.name} from your bag')
 
     request.session['basket'] = basket
-
     return redirect(reverse('view_basket'))
-
-
