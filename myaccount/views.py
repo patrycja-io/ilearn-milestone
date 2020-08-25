1f10056579ca177cd4b60bd51c98d6bd53d9667f
Patrycja <patrycjatyra@protonmail.com>
2020-08-25 21:02:20 +0000

    updated views to my account

--- myaccount/views.py
+++ myaccount/views.py
@@ -1,10 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from .models import UserAccount
 
 
 def myaccount(request):
     """ Display the user's account. """
+    profile = get_object_or_404(UserAccount, user=request.user)
+
 
     template = 'myaccount/myaccount.html'
-    context = {}
+    context = {
+         'myaccount': myaccount,
+    }
 
     return render(request, template, context)
