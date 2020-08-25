886b8dedb3a58a81da82c31d8ff89eddd1cc8896
Patrycja <patrycjatyra@protonmail.com>
2020-08-25 14:38:00 +0000

    added urls contact and terms

---
+++ pages/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .import views
+
+urlpatterns = [
+   path('about/', views.about, name="about"),
+   path('contact/', views.contact, name="contact"),
+   path('terms/', views.terms, name="terms")
+]
