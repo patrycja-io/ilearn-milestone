25579304a9a86519b05170a8b523f89c2d360f3d
Patrycja <patrycjatyra@protonmail.com>
2020-08-25 18:51:51 +0000

    removed about as its part of index now

--- pages/urls.py
+++ pages/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-   path('about/', views.about, name="about"),
    path('contact/', views.contact, name="contact"),
    path('terms/', views.terms, name="terms")
 ]
