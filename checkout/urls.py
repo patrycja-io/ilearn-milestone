258be5b9be6c835798531231e0a1ab93e824c537
Patrycja <patrycjatyra@protonmail.com>
2020-08-26 21:13:57 +0000

    updated urls

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
+from .webhooks import webhook
 
 urlpatterns = [
-    path('', views.checkout, name='checkout')
+    path('', views.checkout, name='checkout'),
+    path('checkout_success/<order_number>', views.checkout_success, name='checkout_success'),
+    path('cache_checkout_data/', views.cache_checkout_data, name='cache_checkout_data'),
+    path('wh/', webhook, name='webhook'),
 ]
