c1bcf85f5084731c9022243dac8219c21346eb89
Patrycja <patrycjatyra@protonmail.com>
2020-08-26 17:02:31 +0000

    updated myviews on my account

--- myaccount/views.py
+++ myaccount/views.py
@@ -1,16 +1,28 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 
 from .models import UserAccount
+from .forms import UserAccountForm
 
 
 def myaccount(request):
-    """ Display the user's account. """
-    profile = get_object_or_404(UserAccount, user=request.user)
+    """ Display the user's profile. """
+    myaccount = get_object_or_404(UserAccount, user=request.user)
 
+    if request.method == 'POST':
+        form = UserAccountForm(request.POST, instance=myaccount)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Account updated successfully')
+
+    form = UserAccountForm(instance=myaccount)
+    orders = myaccount.orders.all()
 
     template = 'myaccount/myaccount.html'
     context = {
-         'myaccount': myaccount,
+        'form': form,
+        'orders': orders,
+        'on_myaccount_page': True
     }
 
     return render(request, template, context)
