05cfe511680d167179589675709e4c1be1feef5c
Patrycja <patrycjatyra@protonmail.com>
2020-08-27 15:31:30 +0000

    added django settings

--- checkout/models.py
+++ checkout/models.py
@@ -2,6 +2,8 @@ import uuid
 
 from django.db import models
 from django.db.models import Sum
+from django.conf import settings
+
 from django_countries.fields import CountryField
 
 from courses.models import Course
