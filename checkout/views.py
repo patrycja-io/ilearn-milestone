2cf65d6b9757b1c3df7d5c717fa8ba8941af3329
Patrycja <patrycjatyra@protonmail.com>
2020-08-27 15:30:56 +0000

    added imports

--- checkout/views.py
+++ checkout/views.py
@@ -1,10 +1,15 @@
-from django.shortcuts import render, redirect, reverse,get_object_or_404, HttpResponse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import HttpResponse
 from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
 
 from .forms import OrderForm
-from .models import Order, OrderLine
+from .models import Order, OrderItem
+
+from myaccount.models import UserAccount
+from myaccount.forms import UserAccountForm
+
 from courses.models import Course
 from basket.context import basket_ebooks
 
@@ -33,31 +38,119 @@ def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
-    basket = request.session.get('basket', {})
-    if not basket:
-        messages.error(request, "Basket is empty!")
-        return redirect(reverse('courses'))
+    if request.method == 'POST':
+        basket = request.session.get('basket', {})
 
-    current_basket = basket_ebooks(request)
-    total = current_basket['total']
-    stripe_total = round(total * 100)
-    stripe.api_key = stripe_secret_key
-    intent = stripe.PaymentIntent.create(
-        amount=stripe_total,
-        currency=settings.STRIPE_CURRENCY,
-    )
+        form_data = {
+            'full_name': request.POST['full_name'],
+            'email': request.POST['email'],
+            'phone_number': request.POST['phone_number'],
+            'country': request.POST['country'],
+            'postcode': request.POST['postcode'],
+            'town_or_city': request.POST['town_or_city'],
+            'street_address1': request.POST['street_address1'],
+            'street_address2': request.POST['street_address2'],
+            'county': request.POST['county'],
+        }
+        order_form = OrderForm(form_data)
+        if order_form.is_valid():
+            order = order_form.save(commit=False)
+            pid = request.POST.get('client_secret').split('_secret')[0]
+            order.stripe_pid = pid
+            order.original_basket = json.dumps(basket)
+            order.save()
+            for course_id, item_data in basket():
+                try:
+                    course = Course.objects.get(id=course_id)
+                    if isinstance(item_data, int):
+                        order_line_item = OrderItem(
+                            order=order,
+                            course=course,
+                        )
+                        order_line_item.save()
+                except Course.DoesNotExist:
+                    messages.error(request, (
+                        "One of the products in your basket wasn't found in our database. "
+                        "Please contact us")
+                    )
+                    order.delete()
+                    return redirect(reverse('view_basket'))
 
-    order_form = OrderForm()
+            request.session['save_info'] = 'save-info' in request.POST
+            return redirect(reverse('checkout_success',
+                                    args=[order.order_number]))
+        else:
+            messages.error(request, 'There was an error with your form. \
+                Please double check your information.')
+    else:
+        basket = request.session.get('basket', {})
+        if not basket:
+            messages.error(request, "There's nothing in your basket yet")
+            return redirect(reverse('courses'))
+
+        current_basket = basket_ebooks(request)
+        total = current_basket['total']
+        stripe_total = round(total * 100)
+        stripe.api_key = stripe_secret_key
+        intent = stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY,
+        )
+
+        order_form = OrderForm()
 
     if not stripe_public_key:
         messages.warning(request, 'Stripe public key is missing. \
             Did you forget to set it in your environment?')
 
-    template = 'checkout.html'
+    template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51HHsLPHe52J6RnjHdH4B4wyvSY2jVlZxZeIABYuPmMtXHJ84S6cW7AruDORAxHFW7eUpcpVjwwTGQR0nPxBk7RXZ00diHfkSEQ',
-        'client_secret': 'test client secret',
+        'stripe_myaccount_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
+    return render(request, template, context)
+
+
+def checkout_success(request, order_number):
+    """
+    Handle successful checkouts
+    """
+    save_info = request.session.get('save_info')
+    order = get_object_or_404(Order, order_number=order_number)
+
+    if request.user.is_authenticated:
+        myaccount = UserAccount.objects.get(user=request.user)
+        # Attach the user's profile to the order
+        order.user_myaccount = myaccount
+        order.save()
+
+        # Save the user's info
+        if save_info:
+            myaccount_data = {
+                'default_phone_number': order.phone_number,
+                'default_country': order.country,
+                'default_postcode': order.postcode,
+                'default_town_or_city': order.town_or_city,
+                'default_street_address1': order.street_address1,
+                'default_street_address2': order.street_address2,
+                'default_county': order.county,
+            }
+            user_myaccount_form = UserAccountForm(myaccount_data,
+                                                instance=myaccount)
+            if user_myaccount_form.is_valid():
+                user_myaccount_form.save()
+
+    messages.success(request, f'Order successfully processed! \
+        Your order number is {order_number}. A confirmation \
+        email will be sent to {order.email}.')
+
+    if 'basket' in request.session:
+        del request.session['basket']
+
+    template = 'checkout/checkout_success.html'
+    context = {
+        'order': order,
+    }
     return render(request, template, context)
