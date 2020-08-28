42ee745f980019166e422f75657009fc5b113698
Patrycja <patrycjatyra@protonmail.com>
2020-08-28 19:24:19 +0000

    updated urls

--- myprofile/urls.py
+++ myprofile/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.myaccount, name='myaccount')
+    path('', views.myaccount, name='myprofile')
+    path('order_history/<order_number>',
+         views.order_history,
+         name='order_history'),
 ]
