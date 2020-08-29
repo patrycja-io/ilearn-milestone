20fc30c017143c5d5f7e6c4b622fd62172c35c7a
Patrycja <patrycjatyra@protonmail.com>
2020-08-29 22:40:51 +0000

    updated urls about payment approved

--- checkout/urls.py
+++ checkout/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.checkout, name='checkout')
+    path('', views.checkout, name='checkout'),
+     path('payment_approved/<order_number>', views.payment_approved, name='payment_approved'),
 ]
 
