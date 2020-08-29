f4aa4fb9d92c44c2ea274ccdb029d886524a3a81
Patrycja <patrycjatyra@protonmail.com>
2020-08-29 21:38:26 +0000

    updated public keys

--- ilearn/settings.py
+++ ilearn/settings.py
@@ -205,8 +205,8 @@ if 'USE_AWS' in os.environ:
 
 
 # Stripe
-STRIPE_CURRENCY = 'usd'
-STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
-STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
-STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
+STRIPE_CURRENCY = 'gbp'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY')
+STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET')
 DEFAULT_FROM_EMAIL = 'ilearn@example.com'
