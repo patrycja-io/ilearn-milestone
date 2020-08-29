1911e51a9f7bbd7b2a3c9ed02f79d178318a7214
Patrycja <patrycjatyra@protonmail.com>
2020-08-29 19:50:28 +0000

    recreated admin.py

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Order, OrderEbook
 
-# Register your models here.
+
+class OrderAdmin(admin.ModelAdmin):
+
+    readonly_fields = ('order_number', 'date',
+                       'total',)
+
+    fields = ('order_number', 'date', 'full_name',
+              'email', 'phone_number', 'country',
+              'postcode', 'town_or_city', 'street_address1',
+              'street_address2', 'county',
+              'total',)
+
+    list_display = ('order_number', 'date', 'full_name',
+                    'total',)
+
+    ordering = ('-date',)
+
+admin.site.register(Order, OrderAdmin)
