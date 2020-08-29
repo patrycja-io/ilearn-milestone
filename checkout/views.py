138a7d736890fd9c1797b71379beedf396acd9e8
Patrycja <patrycjatyra@protonmail.com>
2020-08-29 20:44:20 +0000

    updated views with stripe settings

--- checkout/views.py
+++ checkout/views.py
@@ -2,18 +2,40 @@ from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
 
 from .forms import OrderForm
+from basket.contexts import basket_ebooks
+
+import stripe
 
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     basket = request.session.get('basket', {})
     if not basket:
         messages.error(request, "There's nothing in your basket yet")
         return redirect(reverse('courses'))
 
+    current_basket = basket_ebooks(request)
+    total = current_basket['total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. \
+            Did you forget to set it in your environment?')
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context) 
