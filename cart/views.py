0a7c12a6a69239b303e1a86defc5b3570ffbf216
Patrycja <patrycjatyra@protonmail.com>
2020-08-29 19:17:47 +0000

    added views.py to cart

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
 
-# Create your views here.
+from .forms import OrderForm
+
+
+def cart(request):
+    basket = request.session.get('basket', {})
+    if not basket:
+        messages.error(request, "There's nothing in your basket yet")
+        return redirect(reverse('courses'))
+
+    order_form = OrderForm()
+    template = 'cart/cart.html'
+    context = {
+        'order_form': order_form,
+    }
+
+    return render(request, template, context)
