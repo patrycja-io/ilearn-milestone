2e4628f28a1dbc092d3843f996082fa7e966074a
Patrycja <patrycjatyra@protonmail.com>
2020-08-29 16:34:46 +0000

    linting

--- myprofile/models.py
+++ myprofile/models.py
@@ -12,12 +12,18 @@ class UserAccount(models.Model):
     delivery information and order history
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    default_phone_number = models.CharField(max_length=20, null=True, blank=True)
-    default_country = CountryField(blank_label='Country *', null=True, blank=True)
-    default_postcode = models.CharField(max_length=20, null=True, blank=True)
-    default_town_or_city = models.CharField(max_length=40, null=True, blank=True)
-    default_street_address1 = models.CharField(max_length=80, null=True, blank=True)
-    default_street_address2 = models.CharField(max_length=80, null=True, blank=True)
+    default_phone_number = models.CharField(max_length=20, null=True,
+                                            blank=True)
+    default_country = CountryField(blank_label='Country *', null=True,
+                                   blank=True)
+    default_postcode = models.CharField(max_length=20, null=True,
+                                        blank=True)
+    default_town_or_city = models.CharField(max_length=40, null=True,
+                                            blank=True)
+    default_street_address1 = models.CharField(max_length=80, null=True,
+                                               blank=True)
+    default_street_address2 = models.CharField(max_length=80, null=True,
+                                               blank=True)
     default_county = models.CharField(max_length=80, null=True, blank=True)
 
     def __str__(self):
@@ -29,7 +35,4 @@ def create_update_myprofile(sender, instance, created, **kwargs):
     """
     Create or update the user profile
     """
-    if created:
-        UserAccount.objects.create(user=instance)
-    # Existing users: just save the profile
-    instance.useraccount.save()
+    
