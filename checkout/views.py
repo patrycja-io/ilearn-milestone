79f69b9150e1e90076a5ad21ca2a70647d07ae74
Patrycja <patrycjatyra@protonmail.com>
2020-08-30 21:07:52 +0000

    updated views

--- checkout/views.py
+++ checkout/views.py
@@ -16,6 +16,21 @@ from basket.context import basket_ebooks
 import stripe
 import json
 
+@require_POST
+def cache_data(request):
+    try:
+        pid = request.POST.get('client_secret').split('_secret')[0]
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        stripe.PaymentIntent.modify(pid, metadata={
+            'basket': json.dumps(request.session.get('basket', {})),
+            'save_info': request.POST.get('save_info'),
+            'username': request.user,
+        })
+        return HttpResponse(status=200)
+    except Exception as e:
+        messages.error(request, 'Sorry, your payment cannot be \
+            processed right now. Please try again later.')
+        return HttpResponse(content=e, status=400)
 
 def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
