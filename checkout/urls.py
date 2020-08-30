359ba676210f060d0ea25c028814547f76d5f129
Patrycja <patrycjatyra@protonmail.com>
2020-08-30 20:22:33 +0000

    updated urls

--- checkout/urls.py
+++ checkout/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.checkout, name='checkout'),
-     path('payment_approved/<order_number>', views.payment_approved, name='payment_approved'),
+    path('payment_approved/<order_number>', views.payment_approved, name='payment_approved'),
+    path('cache_checkout_data/', views.cache_checkout_data, name='cache_checkout_data'),
+    path('wh/', webhook, name='webhook'),
 ]
+
 
