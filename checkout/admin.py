3459f50d5b0a515f1bef43707b552253003d0356
Patrycja <patrycjatyra@protonmail.com>
2020-08-30 22:16:39 +0000

    admin stripe bag added

--- checkout/admin.py
+++ checkout/admin.py
@@ -5,13 +5,15 @@ from .models import Order, OrderEbook
 class OrderAdmin(admin.ModelAdmin):
 
     readonly_fields = ('order_number', 'date',
-                       'total',)
+                       'total','original_basket',
+                       'stripe_pid')
 
     fields = ('order_number', 'date', 'full_name',
               'email', 'phone_number', 'country',
               'postcode', 'town_or_city', 'street_address1',
               'street_address2', 'county',
-              'total',)
+              'total','original_basket',
+                       'stripe_pid')
 
     list_display = ('order_number', 'date', 'full_name',
                     'total',)
