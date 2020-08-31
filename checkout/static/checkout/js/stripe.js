cfc9281501937609a07c3ac7b9320628bcc60675
Patrycja <patrycjatyra@protonmail.com>
2020-08-31 13:34:12 +0000

    updated stripe

--- checkout/static/checkout/js/stripe.js
+++ checkout/static/checkout/js/stripe.js
@@ -1,3 +1,14 @@
+
+/*
+    Core logic/payment flow for this comes from here:
+    https://stripe.com/docs/payments/accept-a-payment
+
+    Style from here:  
+    https://stripe.com/docs/stripe-js
+*/
+
+
+
 var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
 var clientSecret = $('#id_client_secret').text().slice(1, -1);
 var stripe = Stripe(stripePublicKey);
@@ -45,6 +56,7 @@ form.addEventListener('submit', function(ev) {
     $('#submit-button').attr('disabled', true);
     $('#payment-form').fadeToggle(100);
 
+    
     var saveInfo = Boolean($('#id-save-info').attr('checked'));
     // From using {% csrf_token %} in the form
     var csrfToken = $('input[name="csrfmiddlewaretoken"]').val();
