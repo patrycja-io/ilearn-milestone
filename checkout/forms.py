4e66c5b675695598daacc936e9295c463686b8c6
Patrycja <patrycjatyra@protonmail.com>
2020-08-31 14:32:51 +0000

    updated checkout forms

--- checkout/forms.py
+++ checkout/forms.py
@@ -30,10 +30,11 @@ class OrderForm(forms.ModelForm):
 
         self.fields['full_name'].widget.attrs['autofocus'] = True
         for field in self.fields:
-            if self.fields[field].required:
-                placeholder = f'{placeholders[field]} *'
-            else:
-                placeholder = placeholders[field]
-            self.fields[field].widget.attrs['placeholder'] = placeholder
+             if field != 'country':
+                if self.fields[field].required:
+                   placeholder = f'{placeholders[field]} *'
+                else:
+                   placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
             self.fields[field].widget.attrs['class'] = 'stripe-style-input'
             self.fields[field].label = False 
