e350ad4c85a075a3c28364cdae7c4c1255918f85
Patrycja <patrycjatyra@protonmail.com>
2020-09-04 12:06:06 +0000

    renamed

--- checkout/models.py
+++ checkout/models.py
@@ -12,7 +12,7 @@ from myprofile.models import UserAccount
 
 class Order(models.Model):
     order_number = models.CharField(max_length=32, null=False, editable=False)
-    user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL,
+    user_account = models.ForeignKey(UserAccount, on_delete=models.SET_NULL,
                                      null=True, blank=True,
                                      related_name='orders')
     full_name = models.CharField(max_length=50, null=False, blank=False)
