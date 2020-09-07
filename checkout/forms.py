f631237de9e94ca6a8c1ab4142b8befa46c91ced
Patrycja <patrycjatyra@protonmail.com>
2020-09-07 17:42:32 +0000

    whitespace

--- checkout/forms.py
+++ checkout/forms.py
@@ -32,9 +32,9 @@ class OrderForm(forms.ModelForm):
         for field in self.fields:
             if field != 'country':
                 if self.fields[field].required:
-                   placeholder = f'{placeholders[field]} *'
+                    placeholder = f'{placeholders[field]} *'
                 else:
-                   placeholder = placeholders[field]
+                    placeholder = placeholders[field]
                 self.fields[field].widget.attrs['placeholder'] = placeholder
             self.fields[field].widget.attrs['class'] = 'stripe-style-input'
-            self.fields[field].label = False 
+            self.fields[field].label = False
