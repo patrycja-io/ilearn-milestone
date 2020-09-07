6b58d48d7f05a3d5310f847d42f499f385487674
Patrycja <patrycjatyra@protonmail.com>
2020-09-07 20:37:31 +0000

    added django reverse

--- profiles/views.py
+++ profiles/views.py
@@ -40,7 +40,6 @@ def order_history(request, order_number):
 
     messages.info(request, (
         f'This is a past confirmation for order number {order_number}. '
-        'A confirmation email was sent on the order date.'
     ))
 
     template = 'checkout/payment_approved.html'
