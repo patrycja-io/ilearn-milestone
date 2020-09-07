639a8a3017a60d2451561ec9c5f24ea91902d2b5
Patrycja <patrycjatyra@protonmail.com>
2020-09-07 20:35:37 +0000

    added formatting to webhooks

--- checkout/webhooks.py
+++ checkout/webhooks.py
@@ -7,6 +7,7 @@ from checkout.webhook_handler import StripeWH_Handler
 
 import stripe
 
+
 @require_POST
 @csrf_exempt
 def webhook(request):
@@ -21,9 +22,9 @@ def webhook(request):
     event = None
 
     try:
-        event = stripe.Webhook.construct_event(
-        payload, sig_header, wh_secret
-        )
+        event = stripe.Webhook.construct_event(payload,
+                                               sig_header,
+                                               wh_secret)
     except ValueError as e:
         # Invalid payload
         return HttpResponse(status=400)
