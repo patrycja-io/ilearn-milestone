a214238b846889c5d87095a1f68c9eb122a77d6d
Patrycja <patrycjatyra@protonmail.com>
2020-09-07 20:34:09 +0000

    added formatting on too long data

--- checkout/models.py
+++ checkout/models.py
@@ -25,7 +25,8 @@ class Order(models.Model):
     street_address2 = models.CharField(max_length=80, null=True, blank=True)
     county = models.CharField(max_length=80, null=True, blank=True)
     date = models.DateTimeField(auto_now_add=True)
-    total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    total = models.DecimalField(max_digits=10, decimal_places=2, null=False, 
+                                default=0)
     original_basket = models.TextField(null=False, blank=False, default='')
     stripe_pid = models.CharField(max_length=254, null=False, blank=False,
                                   default='')
@@ -50,9 +51,12 @@ class Order(models.Model):
 
 
 class OrderEbook(models.Model):
-    order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE)
-    course = models.ForeignKey(Course, null=False, blank=False, on_delete=models.CASCADE)
-    total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
+    order = models.ForeignKey(Order, null=False, blank=False, 
+                              on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, null=False, blank=False, 
+                               on_delete=models.CASCADE)
+    total = models.DecimalField(max_digits=6, decimal_places=2, null=False,
+                                blank=False, editable=False)
 
     def save(self, *args, **kwargs):
         """
