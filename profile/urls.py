05b0f733696651337b4dc9f0d4fc958e4b2d99e9
Patrycja <patrycjatyra@protonmail.com>
2020-09-07 14:22:14 +0000

    deleted my profile app

--- profile/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.myprofile, name='myprofile'),
-    path('order_history/<order_number>',
-         views.order_history,
-         name='order_history'),
-]
