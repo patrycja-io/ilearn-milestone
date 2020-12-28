605b9207aab8cae928f3dc70049811e69a9795d0
patrycja-io <patrycjatyra@protonmail.com>
2020-12-28 20:17:29 +0000

    added virtual environment and other settings

--- ilearn/wsgi.py
+++ ilearn/wsgi.py
@@ -16,5 +16,6 @@ from whitenoise import WhiteNoise
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ilearn.settings')
 
 application = get_wsgi_application()
+
 application = WhiteNoise(application, root='/path/to/static/files')
 application.add_files('/path/to/more/static/files', prefix='more-files/')
