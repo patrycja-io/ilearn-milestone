b2298db03b47be73d946d012ce4948d20c7b6cd6
Patrycja <patrycjatyra@protonmail.com>
2020-12-29 13:34:48 +0000

    changed form media to static

--- ilearn/urls.py
+++ ilearn/urls.py
@@ -27,4 +27,5 @@ urlpatterns = [
     path('basket/', include('basket.urls')),
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
