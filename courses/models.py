c674aed541cfc0ce69f4d918a97b73ea9b2edf63
Patrycja <patrycjatyra@protonmail.com>
2020-12-29 22:16:54 +0000

    changed Course model and managed migrations

--- courses/models.py
+++ courses/models.py
@@ -21,12 +21,12 @@ class Course(models.Model):
                                  on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
+    description = models.TextField(null=True, blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True,
                                  blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
-
 
     def __str__(self):
         return self.name
