6f3eb3228c3541d536534c96d10ee170b5cccf24
Patrycja <patrycjatyra@protonmail.com>
2020-12-29 17:52:38 +0000

    removed coma from model

--- courses/models.py
+++ courses/models.py
@@ -21,12 +21,13 @@ class Course(models.Model):
                                  on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
-    description = models.TextField(),
+    description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     rating = models.DecimalField(max_digits=6, decimal_places=2, null=True,
                                  blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
+
 
     def __str__(self):
         return self.name
