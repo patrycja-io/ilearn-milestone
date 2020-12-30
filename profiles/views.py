c483a6ebfa2d4804f487acbe284bce0b2da170ff
Patrycja <patrycjatyra@protonmail.com>
2020-12-30 17:47:24 +0000

    updated profiles.html

--- profiles/views.py
+++ profiles/views.py
@@ -1,13 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
-from django.contrib.auth.decorators import login_required
 from .models import UserProfile
 from .forms import UserProfileForm
 
 from checkout.models import Order
 
 
-@login_required
+
 def profile(request):
     """ Display the user's profile. """
     profile = get_object_or_404(UserProfile, user=request.user)
@@ -17,12 +16,9 @@ def profile(request):
         if form.is_valid():
             form.save()
             messages.success(request, 'Profile updated successfully')
-        else:
-            messages.error(request,
-                           ('Update failed. Please ensure '
-                            'the form is valid.'))
-    else:
-        form = UserProfileForm(instance=profile)
+       
+        
+    form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
 
     template = 'profiles/profile.html'
