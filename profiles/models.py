d5a6d70ed646c44e1da304d910963fcac73bcfc3
Patrycja <patrycjatyra@protonmail.com>
2020-12-30 01:07:15 +0000

    changed model.py in profiles from UserProfile to userprofile

--- profiles/models.py
+++ profiles/models.py
@@ -39,6 +39,6 @@ def create_or_update_user_profile(sender, instance, created, **kwargs):
     Create or update the user profile
     """
     if created:
-        UserProfile.objects.create(user=instance)
+        userprofile.objects.create(user=instance)
     # Existing users: just save the profile
     instance.userprofile.save()
