5308daffdc70d840062d454b179c0783c95e3969
Patrycja <patrycjatyra@protonmail.com>
2020-12-31 01:05:58 +0000

    fixed basket urls.py

--- basket/urls.py
+++ basket/urls.py
@@ -1,6 +1,5 @@
-from django.contrib import admin
-from django.urls import path, include
-from .import views
+from django.urls import path
+from . import views
 
 urlpatterns = [
     path('', views.view_basket, name='view_basket'),
