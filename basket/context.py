bb5f6e22726a71627c0ccbfe310231816c23e544
Patrycja <patrycjatyra@protonmail.com>
2020-12-31 01:31:45 +0000

    important - changed all logic in context

--- basket/context.py
+++ basket/context.py
@@ -1,23 +1,37 @@
-from decimal import Decimal
-from django.conf import settings
 from django.shortcuts import get_object_or_404
 from courses.models import Course
 
 
 def basket_ebooks(request):
 
-    basket_items = set()
+    basket_items = []
     total = 0
-    basket = request.session.get('basket', {})
+    product_count = 0
+    basket = request.session.get('basket', [])
 
-    for course_id in basket.keys():
+    for values in basket:
+        course_id = values['course']
         course = get_object_or_404(Course, pk=course_id)
-        total += course.price
-        basket_items.add(course)
+        sub_total = course.price
+        total += values['quantity'] * course.price
+
+        product_count += values['quantity']
+        basket_items.append({
+            'sub_total': sub_total,
+            'course_id': course_id,
+            'quantity': values['quantity'],
+            'course': course,
+            'id': values['id'],
+        })
+
+        grand_total = total
+        grand_total = float(total) * 0.8
 
     context = {
         'basket_items': basket_items,
         'total': total,
+        'product_count': product_count,
+        'grand_total': grand_total,
     }
 
     return context
