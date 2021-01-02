76f530c90b3620d7c2c5159aa0843e2d505c795f
Patrycja <patrycjatyra@protonmail.com>
2021-01-02 22:21:01 +0000

    fixing css

--- ilearn/settings.py
+++ ilearn/settings.py
@@ -78,6 +78,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
             os.path.join(BASE_DIR, 'templates', 'allauth'),
         ],
         'APP_DIRS': True,
