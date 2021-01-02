cc7c418790754fe8e6c48c50a1f731ab562fb877
Patrycja <patrycjatyra@protonmail.com>
2021-01-02 20:45:26 +0000

    changed url path to checkout success

--- checkout/urls.py
+++ checkout/urls.py
@@ -5,7 +5,7 @@ from .webhooks import webhook
 urlpatterns = [
     path('', views.checkout, name='checkout'),
     path('payment_approved/<order_number>', views.payment_approved, 
-         name='payment_approved'),
+         name='checkout_success'),
     path('cache_data/', views.cache_data, name='cache_data'),
     path('wh/', webhook, name='webhook'),
 ]
