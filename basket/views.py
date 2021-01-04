3c2acd9c4c53d28ec9a2d352c039306d260e6a9c
Patrycja <patrycjatyra@protonmail.com>
2021-01-04 14:55:46 +0000

    added new remove from basket

--- basket/views.py
+++ basket/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, get_object_or_404, reverse
+from django.shortcuts import render, redirect, get_object_or_404, HttpResponse
 from django.contrib import messages
 
 from courses.models import Course
@@ -14,11 +14,10 @@ def view_basket(request):
     return render(request, 'basket/basket.html', context)
 
 
-
 def add_to_basket(request, item_id):
     """ Add a quantity of the specified product to the shopping basket """
     redirect_url = request.POST.get('redirect_url')
-    basket = request.session.get('basket', {})
+    basket = request.session.get('basket', [])
     course = get_object_or_404(Course, pk=item_id)
 
     if item_id not in basket:
@@ -35,12 +34,23 @@ def add_to_basket(request, item_id):
 def delete_from_basket(request, item_id):
     """Delete item from cart"""
 
-    course = get_object_or_404(Course, pk=item_id)
-    basket = request.session.get('basket', [])
+    try:
+        basket = request.session.get('basket', [])
+        removed_item = None
+        new_basket = []
 
-    if item_id in basket:
-        del basket[item_id]
-        messages.success(request, f'Removed {course.name} from your bag')
+        for item in basket:
+            if item['id'] == int(item_id):
+                removed_item = item
+                continue
+            new_basket.append(item)
 
-    request.session['basket'] = basket
-    return redirect(reverse('view_basket'))
+        request.session['basket'] = new_basket
+        if removed_item:
+            course = Course.objects.get(id=removed_item['course'])
+            messages.success(request, f'Removed {course.name} from your bag')
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        messages.error(request, f'Error removing product {e}')
+        return HttpResponse(status=500)
