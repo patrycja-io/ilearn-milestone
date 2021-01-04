57f47f080ed8a6616e3a13ee9aacf6787345ea3a
Patrycja <patrycjatyra@protonmail.com>
2021-01-04 15:47:48 +0000

    changed back context.py

--- basket/context.py
+++ basket/context.py
@@ -4,38 +4,20 @@ from django.shortcuts import get_object_or_404
 from courses.models import Course
 
 
-def basket_contents(request):
+def basket_ebooks(request):
 
-    basket_items = []
+    basket_items = set()
     total = 0
-    course_count = 0
-    basket = request.session.get('basket', [])
+    basket = request.session.get('basket', {})
 
-    for values in basket:
-        item_id = values['course'],
-        course = get_object_or_404(Course, pk=item_id)
-        sub_total = course.price
-        total += values['quantity'] * course.price
-
-        course_count += values['quantity']
-        basket_items.append({
-            'sub_total': sub_total,
-            'item_id': item_id,
-            'quantity': values['quantity'],
-            'course': course,
-            'id': values['id'],
-        })
-    if total < settings.DISCOUNT:
-        grand_total = total
-    else:
-        grand_total = float(total) * 0.8
+    for course_id in basket.keys():
+        course = get_object_or_404(Course, pk=course_id)
+        total += course.price
+        basket_items.add(course)
 
     context = {
         'basket_items': basket_items,
         'total': total,
-        'course_count': course_count,
-        'grand_total': grand_total,
-        'discount': float(total) - float(grand_total),
     }
 
     return context
