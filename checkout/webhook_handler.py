6f4334e3bfa1243e96f7f14ece704115df13cd21
Patrycja <patrycjatyra@protonmail.com>
2021-01-04 14:13:02 +0000

    changed import on webhook handlers

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -3,7 +3,7 @@ from django.core.mail import send_mail
 from django.template.loader import render_to_string
 from django.conf import settings
 
-from .models import Order, OrderEbook
+from .models import Order, OrderLineItem
 from courses.models import Course
 from profiles.models import UserProfile
 
