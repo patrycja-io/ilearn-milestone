d5d1c3eb24632d87743b5a0891fd35286e02afdd
Patrycja <patrycjatyra@protonmail.com>
2021-01-04 15:16:31 +0000

    changed back everything in context.py

--- basket/context.py
+++ basket/context.py
@@ -1,3 +1,4 @@
+from decimal import Decimal
 from django.conf import settings
 from django.shortcuts import get_object_or_404
 from courses.models import Course
@@ -5,19 +6,19 @@ from courses.models import Course
 
 def basket_contents(request):
 
-    items = []
+    basket_items = []
     total = 0
     course_count = 0
     basket = request.session.get('basket', [])
 
-    for values in basket.items():
+    for values in basket:
         item_id = values['course']
         course = get_object_or_404(Course, pk=item_id)
         sub_total = course.price
         total += values['quantity'] * course.price
 
         course_count += values['quantity']
-        items.append({
+        basket_items.append({
             'sub_total': sub_total,
             'item_id': item_id,
             'quantity': values['quantity'],
@@ -30,7 +31,7 @@ def basket_contents(request):
         grand_total = float(total) * 0.8
 
     context = {
-        'items': items,
+        'basket_items': basket_items,
         'total': total,
         'course_count': course_count,
         'grand_total': grand_total,
