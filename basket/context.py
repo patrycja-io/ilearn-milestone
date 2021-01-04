49f9b248f73a66ddaa28f0811378234237889cc8
Patrycja <patrycjatyra@protonmail.com>
2021-01-04 14:14:49 +0000

    changed context

--- basket/context.py
+++ basket/context.py
@@ -6,18 +6,37 @@ from courses.models import Course
 
 def basket_contents(request):
 
-    basket_items = set()
+    basket_items = []
     total = 0
+    course_count = 0
     basket = request.session.get('basket', [])
 
-    for course_id in basket.keys():
-        course = get_object_or_404(Course, pk=course_id)
-        total += course.price
-        basket_items.add(course)
+    for values in basket:
+        item_id = values['course']
+        course = get_object_or_404(Course, pk=item_id)
+        sub_total = course.price
+        total += values['quantity'] * course.price
+
+        
+        product_count += values['quantity']
+        basket_items.append({
+            'sub_total': sub_total,
+            'item_id': item_id,
+            'quantity': values['quantity'],
+            'course': course,
+            'id': values['id'],
+        })
+    if total < settings.DISCOUNT:
+        grand_total = total
+    else:
+        grand_total = float(total) * 0.8
 
     context = {
         'basket_items': basket_items,
         'total': total,
+        'course_count': course_count,
+        'grand_total': grand_total,
+        'discount': float(total) - float(grand_total),
     }
 
     return context
