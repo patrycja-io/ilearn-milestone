4fe7c1e19626fdd6cbb6ff50bc3157ed187c5f26
Patrycja <patrycjatyra@protonmail.com>
2021-01-04 14:10:59 +0000

    changed import on signals

--- checkout/signals.py
+++ checkout/signals.py
@@ -1,10 +1,10 @@
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 
-from .models import OrderEbook
+from .models import OrderLineItem
 
 
-@receiver(post_save, sender=OrderEbook)
+@receiver(post_save, sender=OrderLineItem)
 def update_on_save(sender, instance, created, **kwargs):
     """
     Update order total on item update/create
@@ -12,7 +12,7 @@ def update_on_save(sender, instance, created, **kwargs):
     instance.order.update_total()
 
 
-@receiver(post_delete, sender=OrderEbook)
+@receiver(post_delete, sender=OrderLineItem)
 def update_on_delete(sender, instance, **kwargs):
     """
     Update order total on item
