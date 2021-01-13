5987b94fadb710e9c290ffddfb2d38215b6d26b9
Patrycja <patrycjatyra@protonmail.com>
2021-01-13 19:47:23 +0000

    changed document from

--- ilearn/settings.py
+++ ilearn/settings.py
@@ -228,7 +228,7 @@ STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
+    DEFAULT_FROM_EMAIL = 'customerservice@ilearn.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
