514e3f3803ebe50ae16de27bb7b53698e7ca6391
Patrycja <patrycjatyra@protonmail.com>
2021-01-14 16:44:29 +0000

    added url adjust basket

--- basket/urls.py
+++ basket/urls.py
@@ -4,5 +4,6 @@ from .import views
 urlpatterns = [
     path('', views.view_basket, name='basket'),
     path('add/<item_id>/', views.add_to_basket, name='add_to_basket'),
-    path('remove/<item_id>/', views.remove_from_basket, name='remove_from_basket),
+    path('adjust/<item_id>/', views.adjust_basket, name='adjust_basket'),
+    path('remove/<item_id>/', views.remove_from_basket, name='remove_from_basket)
 ]
