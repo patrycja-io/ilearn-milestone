7195ee7383a68715dd46cd0718ba3580dc8cb210
Patrycja <patrycjatyra@protonmail.com>
2021-01-14 14:57:45 +0000

    changed name from delete to remove

--- basket/urls.py
+++ basket/urls.py
@@ -4,6 +4,5 @@ from .import views
 urlpatterns = [
     path('', views.view_basket, name='basket'),
     path('add/<item_id>/', views.add_to_basket, name='add_to_basket'),
-    path('delete/<item_id>/', views.delete_from_basket,
-         name='delete_from_basket'),
+    path('remove/<item_id>/', views.remove_from_basket, name='remove_from_basket),
 ]
