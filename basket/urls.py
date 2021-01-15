88c7eeb63342f2e2777914b94be3eae745d4c931
Patrycja <patrycjatyra@protonmail.com>
2021-01-15 16:39:43 +0000

    added backticks

--- basket/urls.py
+++ basket/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.view_basket, name='basket'),
     path('add/<item_id>/', views.add_to_basket, name='add_to_basket'),
-     path('adjust/<item_id>/', views.adjust_basket, name='adjust_basket),
+    path('adjust/<item_id>/', views.adjust_basket, name='adjust_basket'),
     path('remove/<item_id>/', views.remove_from_basket,
          name='remove_from_basket'),
 ]
