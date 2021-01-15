04e776e61caeb953c70ca3178a28297c5a0eac65
Patrycja <patrycjatyra@protonmail.com>
2021-01-15 13:44:24 +0000

    updated urls

--- basket/urls.py
+++ basket/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .import views
+from . import views
 
 urlpatterns = [
     path('', views.view_basket, name='basket'),
     path('add/<item_id>/', views.add_to_basket, name='add_to_basket'),
-    path('remove/<item_id>/', views.remove_from_basket, name='remove_from_basket'),
+    path('remove/<item_id>/', views.remove_from_basket, 
+         name='remove_from_basket'),
 ]
