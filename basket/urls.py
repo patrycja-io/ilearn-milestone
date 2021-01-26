9ab589a701b7041b6783dfcde4a800ab1b192603
Patrycja <patrycjatyra@protonmail.com>
2021-01-26 14:20:02 +0000

    removed basket_tools and adjusted url

--- basket/urls.py
+++ basket/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.view_basket, name='basket'),
+    path('', views.view_basket, name='view_basket'),
     path('add/<item_id>/', views.add_to_basket, name='add_to_basket'),
     path('adjust/<item_id>/', views.adjust_basket, name='adjust_basket'),
     path('remove/<item_id>/', views.remove_from_basket,
